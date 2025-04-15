640a3e868df9815d201262c59333d098bf8a3259
Pedro Henrique Machado <machadop1407@gmail.com>
2025-04-14 18:51:40 -0700

    Update supabase-client.ts

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://zuvjtejbzsqlcokpwvwf.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1dmp0ZWpienNxbGNva3B3dndmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzMjkyMDcsImV4cCI6MjA1OTkwNTIwN30.ppqig4Wh77kTbFqMFK0hY-h6V9tjc32-x4IcYYGghLk"
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
 );
